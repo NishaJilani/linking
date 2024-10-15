ae01ed0778465bdcb0a910da04495ad9b49b8673
Nisha Jilani <nisha03240428081@gmail.com>
2024-10-15 10:25:17 +0500

    linking

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Contact() {
+  return (
+    <div>
+        <h1>Contact Page</h1>
+
+        <Link href="/">Home</Link>
+    </div>
+  );
+}
